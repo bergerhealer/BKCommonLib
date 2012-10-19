5965d52869857f94eee00a160c289b5165b8e662
bergerkiller <bergerkiller@gmail.com>
2012-10-19 12:44:36 +0200

    Fixed cache file ref

--- src/com/bergerkiller/bukkit/common/reflection/classes/RegionFileCacheRef.java
+++ src/com/bergerkiller/bukkit/common/reflection/classes/RegionFileCacheRef.java
@@ -6,11 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import net.minecraft.server.RegionFile;
+import net.minecraft.server.RegionFileCache;
 
 import com.bergerkiller.bukkit.common.reflection.SafeField;
 
 public class RegionFileCacheRef {
-	public static final SafeField<Map<File, Reference<RegionFile>>> filesField = new SafeField<Map<File, Reference<RegionFile>>>(RegionFileCacheRef.class, "a");
+	public static final SafeField<Map<File, Reference<RegionFile>>> filesField = new SafeField<Map<File, Reference<RegionFile>>>(RegionFileCache.class, "a");
 	public static final Map<File, Reference<RegionFile>> FILES;
 	static {
 		FILES = filesField.isValid() ? filesField.get(null) : new HashMap<File, Reference<RegionFile>>();
