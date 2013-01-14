ad67b114dc6539fc333ac895640d1de606e7996f
Lennart <0p1q9o2w@hotmail.nl>
2013-01-14 17:09:37 +0100

    Added NMS class

---
+++ src/main/java/com/bergerkiller/bukkit/common/bases/LongHashSet.java
@@ -0,0 +1,14 @@
+package com.bergerkiller.bukkit.common.bases;
+
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_4_6.util.LongHash;
+
+import com.bergerkiller.bukkit.common.utils.WorldUtil;
+
+public class LongHashSet extends org.bukkit.craftbukkit.v1_4_6.util.LongHashSet {
+	public void addAllChunks(World world) {
+		for (org.bukkit.Chunk chunk : WorldUtil.getChunks(world)) {
+			add(LongHash.toLong(chunk.getX(), chunk.getZ()));
+		}
+	}
+}
