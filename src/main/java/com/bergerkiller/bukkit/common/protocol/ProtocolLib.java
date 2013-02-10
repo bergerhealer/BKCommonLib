6f7f28e7a99167cb26e730a3a0a9b969cc8a20be
Lennart <0p1q9o2w@hotmail.nl>
2013-02-10 18:07:58 +0100

    Done latest changes upon ProtocolLib

--- src/main/java/com/bergerkiller/bukkit/common/protocol/ProtocolLib.java
+++ src/main/java/com/bergerkiller/bukkit/common/protocol/ProtocolLib.java
@@ -1,20 +1,15 @@
 package com.bergerkiller.bukkit.common.protocol;
 
-import java.util.HashMap;
-import java.util.Set;
-
 import org.bukkit.entity.Player;
 
 import net.minecraft.server.v1_4_R1.Packet;
 
 import com.bergerkiller.bukkit.common.internal.CommonPlugin;
 import com.bergerkiller.bukkit.common.utils.PacketUtil;
-import com.comphenix.protocol.Packets;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.ConnectionSide;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.MonitorAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 
@@ -22,33 +17,27 @@ public class ProtocolLib {
 	
 	public static void enable(CommonPlugin plugin_) {
 		ProtocolManager pm = ProtocolLibrary.getProtocolManager();
-		HashMap<Set<Integer>, ConnectionSide> packets = new HashMap<Set<Integer>, ConnectionSide>();
-		packets.put(Packets.Client.getSupported(), ConnectionSide.CLIENT_SIDE);
-		packets.put(Packets.Server.getSupported(), ConnectionSide.SERVER_SIDE);
 		
-		for(Set<Integer> set : packets.keySet()) {
-			pm.addPacketListener(new PacketAdapter(plugin_, packets.get(set),
-					ListenerPriority.NORMAL, set) {
-				@Override
-				public void onPacketReceiving(PacketEvent event) {
-					PacketContainer packet = event.getPacket();
-					Packet vanilla = (Packet)packet.getHandle();
-					Player player = event.getPlayer();
-					
-					if(!PacketUtil.callPacketReceiveEvent(player, vanilla))
-						event.setCancelled(true);
-				}
+		pm.addPacketListener(new MonitorAdapter(plugin_, ConnectionSide.BOTH) {
+			@Override
+			public void onPacketReceiving(PacketEvent event) {
+				PacketContainer packet = event.getPacket();
+				Packet vanilla = (Packet)packet.getHandle();
+				Player player = event.getPlayer();
 				
-				@Override
-				public void onPacketSending(PacketEvent event) {
-					PacketContainer packet = event.getPacket();
-					Packet vanilla = (Packet)packet.getHandle();
-					Player player = event.getPlayer();
-					
-					if(!PacketUtil.callPacketSendEvent(player, vanilla))
-						event.setCancelled(true);
-				}
-			});
-		}
+				if(!PacketUtil.callPacketReceiveEvent(player, vanilla))
+					event.setCancelled(true);
+			}
+
+			@Override
+			public void onPacketSending(PacketEvent event) {
+				PacketContainer packet = event.getPacket();
+				Packet vanilla = (Packet)packet.getHandle();
+				Player player = event.getPlayer();
+				
+				if(!PacketUtil.callPacketSendEvent(player, vanilla))
+					event.setCancelled(true);
+			}
+		});
 	}
 }
