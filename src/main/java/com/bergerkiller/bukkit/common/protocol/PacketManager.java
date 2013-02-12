dece1a92c051ec6d118b07abeeba70e05fb59f1c
Lennart <0p1q9o2w@hotmail.nl>
2013-02-12 15:47:10 +0100

    Fixed Disconnect.Spam kick for no reason

--- src/main/java/com/bergerkiller/bukkit/common/protocol/PacketManager.java
+++ src/main/java/com/bergerkiller/bukkit/common/protocol/PacketManager.java
@@ -1,9 +1,13 @@
 package com.bergerkiller.bukkit.common.protocol;
 
+import net.minecraft.server.v1_4_R1.EntityPlayer;
+
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
+import com.bergerkiller.bukkit.common.Task;
 import com.bergerkiller.bukkit.common.internal.CommonPlugin;
+import com.bergerkiller.bukkit.common.utils.CommonUtil;
 
 public class PacketManager {
 	private CommonPlugin plugin;
@@ -25,5 +29,17 @@ public class PacketManager {
 		}
 		
 		pm.registerEvents(new ProtocolListener(), plugin);
+		
+		new Task(plugin) {
+			@Override
+			public void run() {
+				for(Object obj : CommonUtil.getMCServer().getPlayerList().players) {
+					EntityPlayer player = (EntityPlayer)obj;
+					if(!player.playerConnection.disconnected) {
+						player.playerConnection.d();
+					}
+				}
+			}
+		}.start(1, 1);
 	}
 }
