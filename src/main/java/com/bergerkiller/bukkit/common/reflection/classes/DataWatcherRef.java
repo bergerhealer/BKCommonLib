80924e6ef1742d5f52f7e4c45704e5a641640c73
Lennart <0p1q9o2w@hotmail.nl>
2013-02-16 09:02:58 +0100

    Added new method to DataWatcherRef

--- src/main/java/com/bergerkiller/bukkit/common/reflection/classes/DataWatcherRef.java
+++ src/main/java/com/bergerkiller/bukkit/common/reflection/classes/DataWatcherRef.java
@@ -1,15 +1,33 @@
 package com.bergerkiller.bukkit.common.reflection.classes;
 
+import java.util.List;
+
 import com.bergerkiller.bukkit.common.reflection.ClassTemplate;
 import com.bergerkiller.bukkit.common.reflection.MethodAccessor;
 import com.bergerkiller.bukkit.common.reflection.NMSClassTemplate;
 
+@SuppressWarnings("rawtypes")
 public class DataWatcherRef {
 	public static final ClassTemplate<Object> TEMPLATE = new NMSClassTemplate("DataWatcher");
 	public static final MethodAccessor<Void> write = TEMPLATE.getMethod("a", int.class, Object.class);
+	public static final MethodAccessor<Void> watch = TEMPLATE.getMethod("watch", int.class, Object.class);
+	public static final MethodAccessor<List> returnAllWatched = TEMPLATE.getMethod("c");
+	public static final MethodAccessor<List> unwatchAndReturnAllWatched = TEMPLATE.getMethod("b");
 
 	public static void write(Object datawatcher, int index, Object value) {
 		write.invoke(datawatcher, index, value);
+	}
+	
+	public static void watch(Object datawatcher, int index, Object value) {
+		watch.invoke(index, value);
+	}
+	
+	public static List getAllWatched(Object datawatcher) {
+		return returnAllWatched.invoke(datawatcher);
+	}
+	
+	public static List unwatchAndGetAllWatched(Object datawatcher) {
+		return unwatchAndReturnAllWatched.invoke(datawatcher);
 	}
 
 	public static Object create() {
