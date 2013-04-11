5539f8ea95abce5816b9769a49ac30b8888c0ac7
Irmo van den Berge <bergerkiller@gmail.com>
2013-04-11 13:01:49 +0200

    Changed from the HashSet system to a disable-flatmap system
    Signed-off-by: Irmo van den Berge <bergerkiller@gmail.com>

--- src/main/java/com/bergerkiller/bukkit/common/reflection/classes/LongHashSetRef.java
+++ src/main/java/com/bergerkiller/bukkit/common/reflection/classes/LongHashSetRef.java
@@ -1,21 +1,13 @@
 package com.bergerkiller.bukkit.common.reflection.classes;
 
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
-
-import org.bukkit.craftbukkit.v1_5_R2.util.FlatMap;
-import org.bukkit.craftbukkit.v1_5_R2.util.LongHashSet;
 
 import com.bergerkiller.bukkit.common.Common;
 import com.bergerkiller.bukkit.common.reflection.ClassTemplate;
 import com.bergerkiller.bukkit.common.reflection.FieldAccessor;
 import com.bergerkiller.bukkit.common.reflection.MethodAccessor;
 import com.bergerkiller.bukkit.common.reflection.SafeConstructor;
-import com.bergerkiller.bukkit.common.reflection.SafeDirectMethod;
-import com.bergerkiller.bukkit.common.reflection.SafeMethod;
 import com.bergerkiller.bukkit.common.utils.CommonUtil;
-import com.bergerkiller.bukkit.common.utils.MathUtil;
 
 public class LongHashSetRef {
 	public static final ClassTemplate<?> TEMPLATE = ClassTemplate.create(CommonUtil.getCBClass("util.LongHashSet"));
@@ -26,7 +18,7 @@ public class LongHashSetRef {
 	public static final MethodAccessor<Boolean> contains2 = TEMPLATE.getMethod("contains", int.class, int.class);
 	public static final MethodAccessor<Boolean> contains1 = TEMPLATE.getMethod("contains", long.class);
 	public static final MethodAccessor<Void> remove2 = TEMPLATE.getMethod("remove", int.class, int.class);
-	public static final MethodAccessor<Boolean> remove1;
+	public static final MethodAccessor<Boolean> remove1 = TEMPLATE.getMethod("remove", long.class);
 	public static final MethodAccessor<Void> clear = TEMPLATE.getMethod("clear");
 	public static final MethodAccessor<long[]> toArray = TEMPLATE.getMethod("toArray");
 	public static final MethodAccessor<Long> popFirst = TEMPLATE.getMethod("popFirst");
@@ -40,137 +32,36 @@ public class LongHashSetRef {
 
 	static {
 		if (Common.IS_SPIGOT_SERVER) {
-			// Use the fixed version, Spigots LongHashSet is TERRIBLY inefficient
-			final ClassTemplate<LongHashSetFix> fixTemplate = ClassTemplate.create(LongHashSetFix.class);
-			constructor1 = new SafeConstructor<LongHashSetFix>(null) {
+			// Undo the 'FlatMap' change by Spigot - it is inefficient for our use cases
+			constructor1 = constructor2 = new SafeConstructor<Object>(null) {
+				private final FieldAccessor<Object> flat = TEMPLATE.getField("flat");
+				private final FieldAccessor<Integer> freeEntries = TEMPLATE.getField("freeEntries");
+				private final FieldAccessor<Integer> elements = TEMPLATE.getField("elements");
+				private final FieldAccessor<Integer> modCount = TEMPLATE.getField("modCount");
+				private final FieldAccessor<long[]> values = TEMPLATE.getField("values");
+
 				@Override
 				public boolean isValid() {
 					return true;
 				}
 
 				@Override
-				public LongHashSetFix newInstance(Object... parameters) {
-					LongHashSetFix instance = fixTemplate.newInstanceNull();
-					instance.base = new HashSet<Long>();
+				public Object newInstance(Object... parameters) {
+					Object instance = TEMPLATE.newInstanceNull();
+					final int size = parameters.length == 1 ? Math.max((Integer) parameters[0], 1) : 3;
+					// Set the initial field values
+					values.set(instance, new long[size]);
+					elements.set(instance, 0);
+					freeEntries.set(instance, size);
+					modCount.set(instance, 0);
+					flat.set(instance, SpigotDummyFlatMap.newInstance());
+					// All done!
 					return instance;
-				}
-			};
-			constructor2 = new SafeConstructor<LongHashSetFix>(null) {
-				@Override
-				public boolean isValid() {
-					return true;
-				}
-
-				@Override
-				public LongHashSetFix newInstance(Object... parameters) {
-					LongHashSetFix instance = fixTemplate.newInstanceNull();
-					instance.base = new HashSet<Long>((Integer) parameters[0]);
-					return instance;
-				}
-			};
-			// Remove long method - is private in Spigots implementation!
-			final MethodAccessor<Boolean> removeInternal = TEMPLATE.getMethod("remove", long.class);
-			final FieldAccessor<FlatMap<Object>> flat = TEMPLATE.getField("flat");
-			final MethodAccessor<Void> flat_putter = new SafeMethod<Void>(FlatMap.class, "put", long.class, Object.class);
-			remove1 = new SafeDirectMethod<Boolean>() {
-				@Override
-				public Boolean invoke(Object instance, Object... args) {
-					if (instance instanceof LongHashSetFix) {
-						return ((LongHashSetFix) instance).base.remove(args[0]);
-					} else {
-						// First put it into the flat instance
-						flat_putter.invoke(flat.get(instance), args[0], null);
-						// Then call remove(long)
-						return removeInternal.invoke(instance, args);
-					}
 				}
 			};
 		} else {
 			constructor1 = TEMPLATE.getConstructor();
 			constructor2 = TEMPLATE.getConstructor(int.class);
-			remove1 = TEMPLATE.getMethod("remove", long.class);
-		}
-	}
-
-	private static final class LongHashSetFix extends LongHashSet {
-		public Set<Long> base;
-
-		@Override
-		public boolean add(int msw, int lsw) {
-			return add(MathUtil.longHashToLong(msw, lsw));
-		}
-
-		@Override
-		public boolean add(long value) {
-			return base.add(value);
-		}
-
-		@Override
-		public void clear() {
-			base.clear();
-		}
-
-		@Override
-		public boolean contains(int msw, int lsw) {
-			return contains(MathUtil.longHashToLong(msw, lsw));
-		}
-
-		@Override
-		public boolean contains(long value) {
-			return base.contains(value);
-		}
-
-		@Override
-		public boolean isEmpty() {
-			return base.isEmpty();
-		}
-
-		@Override
-		public Iterator<Long> iterator() {
-			return base.iterator();
-		}
-
-		@Override
-		public long[] popAll() {
-			long[] rval = toArray();
-			clear();
-			return rval;
-		}
-
-		@Override
-		public long popFirst() {
-			final Iterator<Long> iter = iterator();
-			if (!iter.hasNext()) {
-				return 0;
-			}
-			final Long value = iter.next();
-			iter.remove();
-			return value;
-		}
-
-		@Override
-		public void remove(int msw, int lsw) {
-			remove(MathUtil.longHashToLong(msw, lsw));
-		}
-
-		@Override
-		public boolean remove(long value) {
-			return base.remove(value);
-		}
-
-		@Override
-		public int size() {
-			return base.size();
-		}
-
-		@Override
-		public long[] toArray() {
-			long[] rval = new long[base.size()];
-			int i = 0;
-			for (Long value : base) {
-				rval[i++] = value;
-			}
-			return rval;
 		}
 	}
 }
---
+++ src/main/java/com/bergerkiller/bukkit/common/reflection/classes/SpigotDummyFlatMap.java
@@ -0,0 +1,34 @@
+package com.bergerkiller.bukkit.common.reflection.classes;
+
+import org.bukkit.craftbukkit.v1_5_R2.util.FlatMap;
+
+import com.bergerkiller.bukkit.common.reflection.ClassTemplate;
+
+class SpigotDummyFlatMap extends FlatMap<Object> {
+	private static final ClassTemplate<SpigotDummyFlatMap> TEMPLATE = ClassTemplate.create(SpigotDummyFlatMap.class);
+
+	private SpigotDummyFlatMap() {
+	}
+
+	@Override
+	public Object get(long msw, long lsw) {
+		return null;
+	}
+
+	@Override
+	public Object get(long key) {
+		return null;
+	}
+
+	@Override
+	public void put(long msw, long lsw, Object value) {
+	}
+
+	@Override
+	public void put(long key, Object value) {
+	}
+
+	public static FlatMap<Object> newInstance() {
+		return TEMPLATE.newInstanceNull();
+	}
+}
