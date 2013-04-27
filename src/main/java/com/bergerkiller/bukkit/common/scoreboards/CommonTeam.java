e7d876b531d0f4b852f32f10b8de3ef78d24fba8
Lennart <0p1q9o2w@hotmail.nl>
2013-04-27 11:25:06 +0200

    Javadocs, we need moreeee Javadocs

--- src/main/java/com/bergerkiller/bukkit/common/scoreboards/CommonTeam.java
+++ src/main/java/com/bergerkiller/bukkit/common/scoreboards/CommonTeam.java
@@ -34,58 +34,129 @@ public class CommonTeam implements Serializable {
 		this.players = new ArrayList<String>();
 	}
 	
+	/**
+	 * Gets the name of team
+	 * 
+	 * @return Name of team
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * Gets the display name of the team
+	 * 
+	 * @return Display name of team
+	 */
 	public String getDisplayName() {
 		return this.displayName;
 	}
 	
+	/**
+	 * Gets the prefix of the team
+	 * 
+	 * @return Prefix of teeam
+	 */
 	public String getPrefix() {
 		return this.prefix;
 	}
 	
+	/**
+	 * Gets the suffix of the team
+	 * 
+	 * @return Suffix of team
+	 */
 	public String getSuffix() {
 		return this.suffix;
 	}
 	
+	/**
+	 * Gets the friendly fire type for the team
+	 * - ON, pvp enabled
+	 * - OFF, pvp disabled
+	 * - INVICIBLE, everyone looks like ghosts ;)
+	 * 
+	 * @return Friendly fire type of the team
+	 */
 	public FriendlyFireType getFriendlyFireType() {
 		return this.friendlyFire;
 	}
 	
+	/**
+	 * Gets all the players in the team
+	 * 
+	 * @return Players in team
+	 */
 	public List<String> getPlayers() {
 		return this.players;
 	}
 	
+	/**
+	 * Wether or not the team stats should be sent to all players
+	 * 
+	 * @return Sent to everyone?
+	 */
 	public boolean shouldSendToAll() {
 		return this.sendToAll;
 	}
 	
+	/**
+	 * Sets the display name for the team
+	 * 
+	 * @param displayName of the team
+	 */
 	public void setDisplayName(String displayName) {
 		this.displayName = displayName;
 		this.update();
 	}
 	
+	/**
+	 * Sets the prefix for the team
+	 * 
+	 * @param prefix of the team
+	 */
 	public void setPrefix(String prefix) {
 		this.prefix = prefix;
 		this.update();
 	}
 	
+	/**
+	 * Sets the suffix for the team
+	 * 
+	 * @param suffix of the team
+	 */
 	public void setSuffix(String suffix) {
 		this.suffix = suffix;
 		this.update();
 	}
 	
+	/**
+	 * Chanegs the freindly fire type for the team
+	 * - ON, pvp enabled
+	 * - OFF, pvp disabled
+	 * - INVICIBLE, everyone looks like ghosts ;)
+	 * 
+	 * @param friendlyFire of the team
+	 */
 	public void setFriendlyFire(FriendlyFireType friendlyFire) {
 		this.friendlyFire = friendlyFire;
 		this.update();
 	}
 	
+	/**
+	 * Set if we shall sedn the data to all players
+	 * 
+	 * @param sendToAll Send to all?
+	 */
 	public void setSendToAll(boolean sendToAll) {
 		this.sendToAll = sendToAll;
 	}
 	
+	/**
+	 * Add a player to the team
+	 * 
+	 * @param player to add
+	 */
 	public void addPlayer(OfflinePlayer player) {
 		players.add(player.getName());
 		
@@ -105,7 +176,12 @@ public class CommonTeam implements Serializable {
 			}
 		}
 	}
-	
+	 
+	/**
+	 * Remove a player from the team
+	 * 
+	 * @param player to remove
+	 */
 	public void removePlayer(OfflinePlayer player) {
 		players.remove(player.getName());
 		
@@ -126,6 +202,9 @@ public class CommonTeam implements Serializable {
 		}
 	}
 	
+	/**
+	 * Dispaly (create) the team
+	 */
 	public void show() {
 		if(this.sendToAll) {
 			for(Player p : Bukkit.getServer().getOnlinePlayers())
@@ -140,6 +219,9 @@ public class CommonTeam implements Serializable {
 		}
 	}
 	
+	/**
+	 * Hide (remove) the team
+	 */
 	public void hide() {
 		if(this.sendToAll) {
 			for(Player p : Bukkit.getServer().getOnlinePlayers())
@@ -154,6 +236,9 @@ public class CommonTeam implements Serializable {
 		}
 	}
 	
+	/**
+	 * Send chanegs to the team
+	 */
 	private void update() {
 		if(this.sendToAll) {
 			for(Player p : Bukkit.getServer().getOnlinePlayers())
@@ -168,10 +253,21 @@ public class CommonTeam implements Serializable {
 		}
 	}
 	
+	/**
+	 * Send all team data to a speciafic player
+	 * 
+	 * @param player to receive team data
+	 */
 	public void send(Player player) {
 		PacketUtil.sendPacket(player, this.getPacket(0));
 	}
 	
+	/**
+	 * Get the team packet with a cusotm action
+	 * 
+	 * @param action of the packet
+	 * @return Packet
+	 */
 	private CommonPacket getPacket(int action) {
 		CommonPacket packet = new CommonPacket(PacketType.SET_SCOREBOARD_TEAM);
 		packet.write(PacketFields.SET_SCOREBOARD_TEAM.team, this.name);
