d5747414464121e37e8d8222f4a309996f570a17
Irmo van den Berge <bergerkiller@gmail.com>
2013-07-11 22:47:02 +0200

    Fixed support for Spigot
    Signed-off-by: Irmo van den Berge <bergerkiller@gmail.com>

--- src/main/java/com/bergerkiller/bukkit/common/server/SpigotServer.java
+++ src/main/java/com/bergerkiller/bukkit/common/server/SpigotServer.java
@@ -8,12 +8,17 @@ public class SpigotServer extends CraftBukkitServer {
 			return false;
 		}
 		// Check that the Spigot install is available
+		// Method 1 (older): Spigot class in org.bukkit.craftbukkit
 		try {
 			Class.forName(CB_ROOT_VERSIONED + ".Spigot");
 			return true;
-		} catch (ClassNotFoundException ex) {
-			return false;
-		}
+		} catch (ClassNotFoundException ex) {}
+		// Method 2 (newer): Spigot configuration in the org.spigotmc
+		try {
+			Class.forName("org.spigotmc.SpigotConfig");
+			return true;
+		} catch (ClassNotFoundException ex) {}
+		return false;
 	}
 
 	@Override
