019912a06c9627f9d28c279f94c0ddb41733eaad
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2013-11-10 13:43:11 +0100

    Common.VERSION update

--- src/main/java/com/bergerkiller/bukkit/common/Common.java
+++ src/main/java/com/bergerkiller/bukkit/common/Common.java
@@ -22,7 +22,7 @@ public class Common {
 	 * <b>Use getVersion() instead if you want the actual, current version!
 	 * Constants get inlined when compiling!</b>
 	 */
-	public static final int VERSION = 156;
+	public static final int VERSION = 170; //156 stable
 	/**
 	 * The Minecraft package path version BKCommonLib is built against
 	 */
