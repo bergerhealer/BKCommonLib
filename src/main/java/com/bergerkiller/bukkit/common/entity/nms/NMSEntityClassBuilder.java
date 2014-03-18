5f787f593817ab66bbf910c4c170c11cadb4a897
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-03-18 21:28:55 +0100

    Use nms instance

--- src/main/java/com/bergerkiller/bukkit/common/entity/nms/NMSEntityClassBuilder.java
+++ src/main/java/com/bergerkiller/bukkit/common/entity/nms/NMSEntityClassBuilder.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.List;
 
 import com.bergerkiller.bukkit.common.entity.CommonEntity;
+import com.bergerkiller.bukkit.common.internal.CommonNMS;
 import com.bergerkiller.bukkit.common.reflection.ClassBuilder;
 import com.bergerkiller.bukkit.common.reflection.classes.WorldRef;
 import com.bergerkiller.bukkit.common.utils.CommonUtil;
@@ -26,7 +27,7 @@ import com.bergerkiller.bukkit.common.utils.WorldUtil;
  */
 public class NMSEntityClassBuilder extends ClassBuilder {
 	private static final Class<?>[] DEFAULT_CONSTRUCTOR_TYPES = {WorldRef.TEMPLATE.getType()};
-	private static final Object[] DEFAULT_CONSTRUCTOR_ARGS = {WorldUtil.getWorlds().iterator().next()};
+	private static final Object[] DEFAULT_CONSTRUCTOR_ARGS = {CommonNMS.getWorlds().iterator().next()};
 	private final List<Constructor<?>> callbackConstructors = new ArrayList<Constructor<?>>();
 
 	public NMSEntityClassBuilder(Class<?> superClass, Collection<Class<?>> callbackClasses) {
