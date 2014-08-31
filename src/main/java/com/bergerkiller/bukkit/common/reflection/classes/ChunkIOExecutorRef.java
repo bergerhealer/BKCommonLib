d9fd0e2ea190f64c59a43723b76faac83c1dcd74
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-08-31 02:10:22 +0200

    Added ref for chunk io executor

---
+++ src/main/java/com/bergerkiller/bukkit/common/reflection/classes/ChunkIOExecutorRef.java
@@ -0,0 +1,17 @@
+package com.bergerkiller.bukkit.common.reflection.classes;
+
+import java.util.Map;
+
+import org.bukkit.craftbukkit.chunkio.ChunkIOExecutor;
+
+import com.bergerkiller.bukkit.common.reflection.ClassTemplate;
+import com.bergerkiller.bukkit.common.reflection.FieldAccessor;
+import com.bergerkiller.bukkit.common.reflection.SafeField;
+import com.bergerkiller.bukkit.common.utils.CommonUtil;
+
+public class ChunkIOExecutorRef {
+	public static final FieldAccessor<Object> asynchronousExecutor = new SafeField<Object>(ChunkIOExecutor.class, "instance");
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static final ClassTemplate<Object> TEMPLATE = new ClassTemplate(CommonUtil.getCBClass("util.AsynchronousExecutor"));
+	public static final FieldAccessor<Map<?, ?>> tasks = TEMPLATE.getField("tasks");
+}
