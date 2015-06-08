fca82a32a9345c6b6942ee05676c64512820960a
FriwiDev <bblosaurus@web.de>
2015-06-08 13:50:18 +0200

    Fixed EnumGamemode error

--- src/main/java/com/bergerkiller/bukkit/common/reflection/classes/EnumGamemodeRef.java
+++ src/main/java/com/bergerkiller/bukkit/common/reflection/classes/EnumGamemodeRef.java
@@ -1,13 +1,14 @@
 package com.bergerkiller.bukkit.common.reflection.classes;
 
+import net.minecraft.server.v1_8_R3.WorldSettings.EnumGamemode;
+
 import com.bergerkiller.bukkit.common.reflection.ClassTemplate;
 import com.bergerkiller.bukkit.common.reflection.FieldAccessor;
 import com.bergerkiller.bukkit.common.reflection.MethodAccessor;
 import com.bergerkiller.bukkit.common.reflection.NMSClassTemplate;
 
 public class EnumGamemodeRef {
-
-    public static final ClassTemplate<?> TEMPLATE = NMSClassTemplate.create("WorldSettings.EnumGamemode");
-    public static final FieldAccessor<Integer> egmId = TEMPLATE.getField("f");
-    public static final MethodAccessor<Object> getFromId = TEMPLATE.getMethod("getById", int.class);
+	public static final ClassTemplate<?> TEMPLATE = new ClassTemplate(EnumGamemode.class);
+	public static final FieldAccessor<Integer> egmId = TEMPLATE.getField("f");
+	public static final MethodAccessor<Object> getFromId = TEMPLATE.getMethod("getById", int.class);
 }
