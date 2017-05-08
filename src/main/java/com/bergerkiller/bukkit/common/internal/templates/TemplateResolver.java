8d918f943eb34f03c7259b27908ec023418a2644
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2017-05-08 02:12:21 +0200

    Specify classloader when loading template resources

--- src/main/java/com/bergerkiller/bukkit/common/internal/templates/TemplateResolver.java
+++ src/main/java/com/bergerkiller/bukkit/common/internal/templates/TemplateResolver.java
@@ -2,6 +2,7 @@ package com.bergerkiller.bukkit.common.internal.templates;
 
 import java.util.HashMap;
 
+import com.bergerkiller.bukkit.common.Common;
 import com.bergerkiller.mountiplex.reflection.declarations.ClassDeclaration;
 import com.bergerkiller.mountiplex.reflection.declarations.SourceDeclaration;
 import com.bergerkiller.mountiplex.reflection.resolver.ClassDeclarationResolver;
@@ -10,7 +11,7 @@ public class TemplateResolver implements ClassDeclarationResolver {
     private final HashMap<Class<?>, ClassDeclaration> classes = new HashMap<Class<?>, ClassDeclaration>();
 
     public TemplateResolver() {
-        SourceDeclaration sourceDec = SourceDeclaration.parseFromResources("com/bergerkiller/bukkit/common/internal/templates/versions/v1_11_R1.txt");
+        SourceDeclaration sourceDec = SourceDeclaration.parseFromResources(Common.class.getClassLoader(), "com/bergerkiller/bukkit/common/internal/templates/versions/v1_11_R1.txt");
         for (ClassDeclaration cdec : sourceDec.classes) {
             classes.put(cdec.type.type, cdec);
         }
