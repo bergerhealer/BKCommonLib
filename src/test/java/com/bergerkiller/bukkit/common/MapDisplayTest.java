33fce439f68594c132e4ed44a9a4260f61598032
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2017-07-20 16:11:15 +0200

    Add a colormap field view image generator + field image

--- src/test/java/com/bergerkiller/bukkit/common/MapDisplayTest.java
+++ src/test/java/com/bergerkiller/bukkit/common/MapDisplayTest.java
@@ -1,15 +1,61 @@
 package com.bergerkiller.bukkit.common;
 
 import java.awt.Color;
+import java.awt.image.BufferedImage;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+
+import javax.imageio.ImageIO;
 
 import org.junit.Test;
 
 import com.bergerkiller.bukkit.common.map.MapColorPalette;
+import com.bergerkiller.mountiplex.MountiplexUtil;
 
 public class MapDisplayTest {
 
+    //@Test
+    public void createColorPaletteFieldImage() {
+        try {
+            // Creates a 4096x4096 .png image of all available colors in the palette.
+            // This helps to verify correct functioning of color to colormap index conversion
+            BufferedImage img = new BufferedImage(4096, 4096, BufferedImage.TYPE_INT_RGB);
+            {
+                HashSet<Integer> coll = new HashSet<Integer>();
+                for (int y = 0; y < 4096; y++) {
+                    for (int x = 0; x < 4096; x++) {
+                        int r = (x & 0xFF);
+                        int g = (y & 0xFF);
+                        int b_1 = ((y >> 8) & 0xF);
+                        int b_2 = ((x >> 8) & 0xF);
+                        if ((b_2 & 0x1) != 0x0) {
+                            r = 255 - r;
+                        }
+                        if ((b_1 & 0x1) != 0x0) {
+                            g = 255 - g;
+                        }
+                        int b = b_2 | (b_1 << 4);
+                        
+                        byte c = MapColorPalette.getColor(r, g, b);
+                        Color cc = MapColorPalette.getRealColor(c);
+                        r = cc.getRed();
+                        g = cc.getGreen();
+                        b = cc.getBlue();
+                        
+                        int rgb = (r << 0) | (g << 8) | (b << 16);
+                        img.setRGB(x, y, rgb);
+                    }
+                }
+            }
+            ImageIO.write(img, "png", new File("misc/map_palette_field.png"));
+        } catch (Throwable t) {
+            throw MountiplexUtil.uncheckedRethrow(t);
+        }
+    }
+    
     //@Test
     public void exportColorPalette() {
         // Exports all color palette entries to an index table in HTML format
