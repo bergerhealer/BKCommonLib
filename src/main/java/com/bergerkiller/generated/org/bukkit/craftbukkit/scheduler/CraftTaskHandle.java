c51fe33f676b6a1413dd4a3fa70756d1a3a6b632
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2018-12-16 20:37:14 +0100

    Add support for new deobfuscation changes in upstream spigot server

---
+++ src/main/java/com/bergerkiller/generated/org/bukkit/craftbukkit/scheduler/CraftTaskHandle.java
@@ -0,0 +1,40 @@
+package com.bergerkiller.generated.org.bukkit.craftbukkit.scheduler;
+
+import com.bergerkiller.mountiplex.reflection.util.StaticInitHelper;
+import com.bergerkiller.mountiplex.reflection.declarations.Template;
+import org.bukkit.plugin.Plugin;
+
+/**
+ * Instance wrapper handle for type <b>org.bukkit.craftbukkit.scheduler.CraftTask</b>.
+ * To access members without creating a handle type, use the static {@link #T} member.
+ * New handles can be created from raw instances using {@link #createHandle(Object)}.
+ */
+public abstract class CraftTaskHandle extends Template.Handle {
+    /** @See {@link CraftTaskClass} */
+    public static final CraftTaskClass T = new CraftTaskClass();
+    static final StaticInitHelper _init_helper = new StaticInitHelper(CraftTaskHandle.class, "org.bukkit.craftbukkit.scheduler.CraftTask");
+
+    /* ============================================================================== */
+
+    public static CraftTaskHandle createHandle(Object handleInstance) {
+        return T.createHandle(handleInstance);
+    }
+
+    /* ============================================================================== */
+
+    public abstract Runnable getTask();
+    public abstract void setTask(Runnable value);
+    public abstract Plugin getPlugin();
+    public abstract void setPlugin(Plugin value);
+    /**
+     * Stores class members for <b>org.bukkit.craftbukkit.scheduler.CraftTask</b>.
+     * Methods, fields, and constructors can be used without using Handle Objects.
+     */
+    public static final class CraftTaskClass extends Template.Class<CraftTaskHandle> {
+        public final Template.Field<Runnable> task = new Template.Field<Runnable>();
+        public final Template.Field<Plugin> plugin = new Template.Field<Plugin>();
+
+    }
+
+}
+
--- src/main/java/com/bergerkiller/reflection/org/bukkit/craftbukkit/CBCraftTask.java
+++ src/main/java/com/bergerkiller/reflection/org/bukkit/craftbukkit/CBCraftTask.java
@@ -1,14 +1,17 @@
 package com.bergerkiller.reflection.org.bukkit.craftbukkit;
 
+import com.bergerkiller.generated.org.bukkit.craftbukkit.scheduler.CraftTaskHandle;
 import com.bergerkiller.mountiplex.reflection.ClassTemplate;
 import com.bergerkiller.mountiplex.reflection.FieldAccessor;
 
 import org.bukkit.plugin.Plugin;
 
+/**
+ * <b>Deprecated: use {@link CraftTaskHandle} instead</b>
+ */
+@Deprecated
 public class CBCraftTask {
-    public static final ClassTemplate<?> T = ClassTemplate.createCB("scheduler.CraftTask")
-    		.addImport("org.bukkit.plugin.Plugin");
-    
-    public static final FieldAccessor<Runnable> task = T.selectField("private final Runnable task");
-    public static final FieldAccessor<Plugin> plugin = T.selectField("private final Plugin plugin");
+    public static final ClassTemplate<?> T = ClassTemplate.create(CraftTaskHandle.T.getType());
+    public static final FieldAccessor<Runnable> task = CraftTaskHandle.T.task.toFieldAccessor();
+    public static final FieldAccessor<Plugin> plugin = CraftTaskHandle.T.plugin.toFieldAccessor();
 }
