336b85722864d66261949bab4e90e0839fcfe3ed
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2020-03-07 21:00:10 +0100

    Improve performance of DataWatcher set (with disabled entity)

--- src/main/java/com/bergerkiller/bukkit/common/internal/CommonDisabledEntity.java
+++ src/main/java/com/bergerkiller/bukkit/common/internal/CommonDisabledEntity.java
@@ -1,41 +1,38 @@
 package com.bergerkiller.bukkit.common.internal;
 
+import java.lang.reflect.Method;
+
+import com.bergerkiller.generated.net.minecraft.server.DataWatcherObjectHandle;
 import com.bergerkiller.generated.net.minecraft.server.EntityHandle;
 import com.bergerkiller.generated.net.minecraft.server.EntityItemHandle;
-import com.bergerkiller.mountiplex.reflection.ClassTemplate;
+import com.bergerkiller.mountiplex.reflection.ClassInterceptor;
+import com.bergerkiller.mountiplex.reflection.Invokable;
 
 public class CommonDisabledEntity {
     public static final EntityHandle INSTANCE;
 
     static {
-        INSTANCE = EntityHandle.createHandle(ClassTemplate.create(EntityItemHandle.T.getType()).newInstanceNull());
+        Object entity = EntityItemHandle.T.newInstanceNull();
+
+        // Since Minecraft 1.14 the Pose datawatcher item is used to refresh the entity size
+        // This adds unwanted overhead, so get rid of it
+        if (CommonBootstrap.evaluateMCVersion(">=", "1.14")) {
+            entity = new ClassInterceptor() {
+                @Override
+                protected Invokable getCallback(Method method) {
+                    if (method.getName().equals("a") && method.getParameterCount() == 1) {
+                        Class<?> argType = method.getParameters()[0].getType();
+                        if (DataWatcherObjectHandle.T.isAssignableFrom(argType)) {
+                            System.out.println("DO NOTHING");
+                            return new NullInvokable();
+                        }
+                    }
+
+                    return null;
+                }
+            }.hook(entity);
+        }
+
+        INSTANCE = EntityHandle.createHandle(entity);
     }
-
-    //TODO: Really needed to override the below methods?
-    // If so, generate an extended type at runtime.
-
-    /*
-    //public static final CommonDisabledEntity INSTANCE = ClassTemplate.create(CommonDisabledEntity.class).newInstanceNull();
-
-    private CommonDisabledEntity() {
-        super(null);
-    }
-
-    @Override
-    protected void a(NBTTagCompound arg0) {
-    }
-
-    @Override
-    protected void b(NBTTagCompound arg0) {
-    }
-
-	@Override
-	public MinecraftServer B_() {
-		return null;
-	}
-
-	@Override
-	protected void i() {
-	}
-	*/
 }
