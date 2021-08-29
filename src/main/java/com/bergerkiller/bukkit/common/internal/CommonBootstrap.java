3a4f770e2547ac49ca493d99a1212d1935c95ec2
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2021-08-29 19:00:22 +0200

    Remapping should always be done, also for 1.16.5

--- src/main/java/com/bergerkiller/bukkit/common/internal/CommonBootstrap.java
+++ src/main/java/com/bergerkiller/bukkit/common/internal/CommonBootstrap.java
@@ -522,19 +522,17 @@ public class CommonBootstrap {
         }
 
         // Tuinity Starlight was migrated into paper, but before that, it sat in its own namespace
-        if (evaluateMCVersion(">=", "1.17.1")) {
-            try {
-                MPLType.getClassByName("com.tuinity.tuinity.chunk.light.SWMRNibbleArray");
+        try {
+            MPLType.getClassByName("com.tuinity.tuinity.chunk.light.SWMRNibbleArray");
 
-                // Found, remap the paper namespace to here
-                for (String name : new String[] {
-                        "SWMRNibbleArray", "StarLightInterface", "StarLightEngine",
-                        "SkyStarLightEngine", "BlockStarLightEngine"
-                }) {
-                    remappings.put("ca.spottedleaf.starlight.light." + name, "com.tuinity.tuinity.chunk.light." + name);
-                }
-            } catch (ClassNotFoundException ex) {}
-        }
+            // Found, remap the paper namespace to here
+            for (String name : new String[] {
+                    "SWMRNibbleArray", "StarLightInterface", "StarLightEngine",
+                    "SkyStarLightEngine", "BlockStarLightEngine"
+            }) {
+                remappings.put("ca.spottedleaf.starlight.light." + name, "com.tuinity.tuinity.chunk.light." + name);
+            }
+        } catch (ClassNotFoundException ex) {}
 
         // If remappings exist, add a resolver for them
         if (!remappings.isEmpty()) {
