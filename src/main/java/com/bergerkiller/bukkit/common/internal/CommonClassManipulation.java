9e0db01b3d6caf3cbaef501dd3d64ae885c751e7
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2022-01-18 18:59:51 +0100

    Use GeneratorClassLoader when rewriting classes
    Fixes bug on MC 1.12.2 and before on modern JDKs

--- src/main/java/com/bergerkiller/bukkit/common/internal/CommonClassManipulation.java
+++ src/main/java/com/bergerkiller/bukkit/common/internal/CommonClassManipulation.java
@@ -2,70 +2,52 @@ package com.bergerkiller.bukkit.common.internal;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.security.CodeSigner;
-import java.security.CodeSource;
 import java.util.Map;
-import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.jar.Manifest;
 
 import org.bukkit.plugin.java.JavaPluginLoader;
 
 import com.bergerkiller.mountiplex.reflection.SafeField;
-import com.bergerkiller.mountiplex.reflection.SafeMethod;
+import com.bergerkiller.mountiplex.reflection.util.GeneratorClassLoader;
 import com.google.common.io.ByteStreams;
 
 /**
  * Helper class for performing plugin class modification
  */
 public class CommonClassManipulation {
-    private static SafeMethod<Package> cl_getPackage_method = new SafeMethod<Package>(ClassLoader.class, "getPackage", String.class);
-    private static SafeMethod<Void> cl_definePackage_method1 = new SafeMethod<Void>(URLClassLoader.class, "definePackage",
-            String.class, Manifest.class, URL.class);
-    private static SafeMethod<Void> cl_definePackage_method2 = new SafeMethod<Void>(URLClassLoader.class, "definePackage",
-            String.class, String.class, String.class, String.class, String.class, String.class, String.class, URL.class);
-    private static SafeMethod<Class<?>> cl_defineClass_method1 = new SafeMethod<Class<?>>(URLClassLoader.class, "defineClass",
-            String.class, byte[].class, int.class, int.class, CodeSource.class);
-    private static SafeMethod<Class<?>> cl_defineClass_method2 = new SafeMethod<Class<?>>(ClassLoader.class, "defineClass",
-            String.class, byte[].class, int.class, int.class);
 
+    @SuppressWarnings("deprecation")
     public static void writeClassData(ClassLoader classLoader, String name, byte[] classData) {
+        // Use the generator class loader to generate new classes
+        GeneratorClassLoader loader = GeneratorClassLoader.get(classLoader);
+
         Class<?> loadedClass;
         if (SafeField.contains(classLoader.getClass(), "jar", JarFile.class)) {
             // PluginClassLoader signature A (New versions of CraftBukkit)
 
-            JarFile jar = SafeField.get(classLoader, "jar", JarFile.class);
-            URL url = SafeField.get(classLoader, "url", URL.class);
-            Manifest manifest = SafeField.get(classLoader, "manifest", Manifest.class);
-            JarEntry entry = jar.getJarEntry(findClassPath(name));
+            // All these fields are inside PluginClassLoader, not ClassLoader
+            //JarFile jar = SafeField.get(classLoader, "jar", JarFile.class);
+            //URL url = SafeField.get(classLoader, "url", URL.class);
+            //Manifest manifest = SafeField.get(classLoader, "manifest", Manifest.class);
+            //JarEntry entry = jar.getJarEntry(findClassPath(name));
 
             // Define package if needed
             int dot = name.lastIndexOf('.');
             if (dot != -1) {
                 String pkgName = name.substring(0, dot);
-                if (cl_getPackage_method.invoke(classLoader, pkgName) == null) {
+                if (loader.getPackage(pkgName) == null) {
                     try {
-                        if (manifest != null) {
-                            cl_definePackage_method1.invoke(classLoader, pkgName, manifest, url);
-                        } else {
-                            cl_definePackage_method2.invoke(classLoader, pkgName, null, null, null, null, null, null, null);
-                        }
-                    } catch (IllegalArgumentException ex) {
-                        if (cl_getPackage_method.invoke(classLoader, pkgName) == null) {
-                            throw new IllegalStateException("Cannot find package " + pkgName);
-                        }
-                    }
+                        loader.definePackage(pkgName, null, null, null, null, null, null, null);
+                    } catch (IllegalArgumentException ex) {}
                 }
             }
 
-            CodeSigner[] signers = entry.getCodeSigners();
-            CodeSource source = new CodeSource(url, signers);
-            loadedClass = cl_defineClass_method1.invoke(classLoader, name, classData, 0, classData.length, source);
+            //CodeSigner[] signers = entry.getCodeSigners();
+            //CodeSource source = new CodeSource(url, signers);
+            loadedClass = loader.createClassFromBytecode(name, classData, null, false);
         } else {
             // PluginClassLoader signature B (Spigot)
-            loadedClass = cl_defineClass_method2.invoke(classLoader, name, classData, 0, classData.length);
+            loadedClass = loader.createClassFromBytecode(name, classData, null, false);
         }
 
         writeClass(classLoader, name, loadedClass);
