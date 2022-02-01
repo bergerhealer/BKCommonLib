14567e7ae52702276ce322b98310b2ab0123c31a
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2022-02-01 19:17:04 +0100

    Use requirements for the queued packet constructor logic

--- src/main/java/com/bergerkiller/generated/net/minecraft/network/NetworkManagerHandle.java
+++ src/main/java/com/bergerkiller/generated/net/minecraft/network/NetworkManagerHandle.java
@@ -25,48 +25,6 @@ public abstract class NetworkManagerHandle extends Template.Handle {
     }
 
     public abstract boolean isConnected();
-
-    private static final java.lang.reflect.Constructor _queuedPacketConstructor;
-    private static final Throwable _queuedPacketConstructorError;
-    static {
-        java.lang.reflect.Constructor c = null;
-        Throwable error = null;
-        try {
-            Class<?> queuedPacketType = com.bergerkiller.bukkit.common.utils.CommonUtil.getClass("net.minecraft.network.NetworkManager$QueuedPacket");
-            Class<?> listenerType = com.bergerkiller.bukkit.common.utils.CommonUtil.getClass("io.netty.util.concurrent.GenericFutureListener");
-            if (queuedPacketType == null) {
-                throw new IllegalStateException("Class QueuedPacket does not exist");
-            }
-            if (listenerType == null) {
-                throw new IllegalStateException("Class GenericFutureListener does not exist");
-            }
-            Class<?> packetType = com.bergerkiller.generated.net.minecraft.network.protocol.PacketHandle.T.getType();
-            if (com.bergerkiller.bukkit.common.internal.CommonBootstrap.evaluateMCVersion(">=", "1.13")) {
-                c = queuedPacketType.getDeclaredConstructor(packetType, listenerType);
-            } else {
-                listenerType = com.bergerkiller.bukkit.common.utils.LogicUtil.getArrayType(listenerType);
-                c = queuedPacketType.getDeclaredConstructor(packetType, listenerType);
-            }
-            c.setAccessible(true);
-        } catch (Throwable t) {
-            error = t;
-            com.bergerkiller.bukkit.common.Logging.LOGGER_REFLECTION.log(java.util.logging.Level.SEVERE, "Failed to find queued packet constructor", t);
-        }
-        _queuedPacketConstructor = c;
-        _queuedPacketConstructorError = error;
-    }
-
-    public static Object createQueuedPacket(Object packet) {
-        try {
-            return _queuedPacketConstructor.newInstance(packet, null);
-        } catch (Throwable t) {
-            if (_queuedPacketConstructorError != null) { // NPE
-                throw new UnsupportedOperationException("Failed to find queued packet constructor", _queuedPacketConstructorError);
-            } else {
-                throw com.bergerkiller.mountiplex.MountiplexUtil.uncheckedRethrow(t);
-            }
-        }
-    }
     public abstract Channel getChannel();
     public abstract void setChannel(Channel value);
     /**
