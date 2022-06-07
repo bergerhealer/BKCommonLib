3c3bae44c6f9b95d0879ba5cd29462864d86a383
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2022-06-07 21:00:58 +0200

    Working towards 1.19-v1

--- src/main/java/com/bergerkiller/bukkit/common/Common.java
+++ src/main/java/com/bergerkiller/bukkit/common/Common.java
@@ -43,7 +43,7 @@ public class Common {
      * <b>Use getVersion() instead if you want the actual, current version!
      * Constants get inlined when compiling!</b>
      */
-    public static final int VERSION = 11802;
+    public static final int VERSION = 11900;
     /**
      * Defines the Minecraft version that runs on the server.
      */
