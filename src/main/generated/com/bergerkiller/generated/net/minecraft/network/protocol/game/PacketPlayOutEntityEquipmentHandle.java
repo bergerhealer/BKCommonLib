422fcd2de6befa57d73b724b920e71c024413fba
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2023-07-24 22:39:15 +0200

    Add an API to the Equipment packet to fix broken serialization on 1.8
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@ziggo.nl>

--- src/main/generated/com/bergerkiller/generated/net/minecraft/network/protocol/game/PacketPlayOutEntityEquipmentHandle.java
+++ src/main/generated/com/bergerkiller/generated/net/minecraft/network/protocol/game/PacketPlayOutEntityEquipmentHandle.java
@@ -22,15 +22,44 @@ public abstract class PacketPlayOutEntityEquipmentHandle extends PacketHandle {
 
     /* ============================================================================== */
 
-    public static PacketPlayOutEntityEquipmentHandle createNew(int entityId, EquipmentSlot slot, ItemStack itemStack) {
-        return T.createNew.invoke(entityId, slot, itemStack);
+    public static PacketPlayOutEntityEquipmentHandle createNew(OwnerType ownerType, int entityId, EquipmentSlot slot, ItemStack itemStack) {
+        return T.createNew.invoke(ownerType, entityId, slot, itemStack);
     }
 
     public abstract int getSlotCount();
-    public abstract EquipmentSlot getEquipmentSlot(int index);
-    public abstract void setEquipmentSlot(int index, EquipmentSlot slot);
+    public abstract EquipmentSlot getEquipmentSlot(OwnerType ownerType, int index);
+    public abstract void setEquipmentSlot(OwnerType ownerType, int index, EquipmentSlot slot);
     public abstract ItemStack getItemStack(int index);
     public abstract void setItemStack(int index, ItemStack itemStack);
+
+    public enum OwnerType {
+        NON_PLAYER(false), PLAYER(true);
+
+        private final boolean isPlayer;
+
+        OwnerType(boolean isPlayer) {
+            this.isPlayer = isPlayer;
+        }
+
+        public boolean isPlayer() {
+            return isPlayer;
+        }
+    }
+
+    @Deprecated
+    public static PacketPlayOutEntityEquipmentHandle createNew(int entityId, org.bukkit.inventory.EquipmentSlot slot, org.bukkit.inventory.ItemStack itemStack) {
+        return createNew(OwnerType.NON_PLAYER, entityId, slot, itemStack);
+    }
+
+    @Deprecated
+    public org.bukkit.inventory.EquipmentSlot getEquipmentSlot(int index) {
+        return getEquipmentSlot(OwnerType.NON_PLAYER, index);
+    }
+
+    @Deprecated
+    public void setEquipmentSlot(int index, org.bukkit.inventory.EquipmentSlot slot) {
+        setEquipmentSlot(OwnerType.NON_PLAYER, index, slot);
+    }
     public abstract int getEntityId();
     public abstract void setEntityId(int value);
     /**
--- src/main/java/com/bergerkiller/bukkit/common/internal/proxy/EnumItemSlot.java
+++ src/main/java/com/bergerkiller/bukkit/common/internal/proxy/EnumItemSlot.java
@@ -4,12 +4,26 @@ package com.bergerkiller.bukkit.common.internal.proxy;
  * Provides the EnumItemSlot enumeration for MC 1.8.8
  */
 public enum EnumItemSlot {
-    MAINHAND(EnumItemSlot.Function.HAND, 0, 0, "mainhand"), FEET(EnumItemSlot.Function.ARMOR, 0, 1, "feet"), LEGS(EnumItemSlot.Function.ARMOR, 1, 2, "legs"), CHEST(EnumItemSlot.Function.ARMOR, 2, 3, "chest"), HEAD(EnumItemSlot.Function.ARMOR, 3, 4, "head");
+    MAINHAND(EnumItemSlot.Function.HAND, 0, 0, "mainhand"),
+    FEET(EnumItemSlot.Function.ARMOR, 0, 1, "feet"),
+    LEGS(EnumItemSlot.Function.ARMOR, 1, 2, "legs"),
+    CHEST(EnumItemSlot.Function.ARMOR, 2, 3, "chest"),
+    HEAD(EnumItemSlot.Function.ARMOR, 3, 4, "head");
 
     private final EnumItemSlot.Function g;
     private final int h;
     private final int i;
     private final String j;
+
+    private static final EnumItemSlot[] BY_PLAYER_SLOT_INDEX = new EnumItemSlot[4];
+    static {
+        BY_PLAYER_SLOT_INDEX[FEET.b()] = FEET;
+        BY_PLAYER_SLOT_INDEX[LEGS.b()] = LEGS;
+        BY_PLAYER_SLOT_INDEX[CHEST.b()] = CHEST;
+        BY_PLAYER_SLOT_INDEX[HEAD.b()] = HEAD;
+    }
+
+    private static final EnumItemSlot[] BY_NONPLAYER_SLOT_INDEX = EnumItemSlot.values(); // Same order
 
     private EnumItemSlot(EnumItemSlot.Function enumitemslot_function, int i, int j, String s) {
         this.g = enumitemslot_function;
@@ -22,16 +36,38 @@ public enum EnumItemSlot {
         return this.g;
     }
 
+    // Slot index when worn by a player ('human')
     public int b() {
         return this.h;
     }
 
+    // Slot index when worn by non-players that lack inventory (armorstands)
     public int c() {
         return this.i;
     }
 
     public String d() {
         return this.j;
+    }
+
+    // Added by BKCL
+    public static EnumItemSlot fromPlayerSlotIndex(int index) {
+        int len = BY_PLAYER_SLOT_INDEX.length;
+        if (index < 0 || index >= len) {
+            return HEAD;
+        } else {
+            return BY_PLAYER_SLOT_INDEX[index];
+        }
+    }
+
+    // Added by BKCL
+    public static EnumItemSlot fromNonPlayerSlotIndex(int index) {
+        int len = BY_NONPLAYER_SLOT_INDEX.length;
+        if (index < 0 || index >= len) {
+            return HEAD;
+        } else {
+            return BY_NONPLAYER_SLOT_INDEX[index];
+        }
     }
 
     public static EnumItemSlot a(String s) {
