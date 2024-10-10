6aee79ab406fdc5db8082893bb3ae1db01e5494b
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2024-10-10 15:57:39 +0200

    Add support for leaf server 1.20
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@ziggo.nl>

--- src/main/java/com/bergerkiller/bukkit/common/internal/logic/RegionHandler_Vanilla_1_14.java
+++ src/main/java/com/bergerkiller/bukkit/common/internal/logic/RegionHandler_Vanilla_1_14.java
@@ -280,7 +280,10 @@ class RegionHandler_Vanilla_1_14 extends RegionHandlerVanilla {
         /*
          * <FIND_REGION_FILE_CACHE_STORAGE>
          * public static Long2ObjectLinkedOpenHashMap findRegionFileCache(RegionFileCache rfc) {
-         * #if exists net.minecraft.world.level.chunk.storage.RegionFileCache public final Long2ObjectLinkedOpenHashMap<org.stupidcraft.linearpaper.region.IRegionFile> regionCache;
+         * #if exists net.minecraft.world.level.chunk.storage.RegionFileCache public final Long2ObjectLinkedOpenHashMap<org.purpurmc.purpur.region.AbstractRegionFile> regionCache;
+         *     // Used on Leaf server (Purpur) (1.20)
+         *     #require net.minecraft.world.level.chunk.storage.RegionFileCache public final Long2ObjectLinkedOpenHashMap<org.purpurmc.purpur.region.AbstractRegionFile> regionCache;
+         * #elseif exists net.minecraft.world.level.chunk.storage.RegionFileCache public final Long2ObjectLinkedOpenHashMap<org.stupidcraft.linearpaper.region.IRegionFile> regionCache;
          *     // Used on Leaf server (1.21)
          *     #require net.minecraft.world.level.chunk.storage.RegionFileCache public final Long2ObjectLinkedOpenHashMap<org.stupidcraft.linearpaper.region.IRegionFile> regionCache;
          * #elseif version >= 1.17
