4a55026487707d642a80c1ba0837326e0fa623cd
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2025-03-17 09:52:41 +0100

    Remove bad import
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@ziggo.nl>

--- src/main/java/com/bergerkiller/bukkit/common/map/util/ItemModel.java
+++ src/main/java/com/bergerkiller/bukkit/common/map/util/ItemModel.java
@@ -8,7 +8,6 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-import io.leangen.geantyref.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
