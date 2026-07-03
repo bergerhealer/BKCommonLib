f62b0c43c16f6ae48fae7b23ce966ef887eb750f
Irmo van den Berge <irmo.vandenberge@mailbox.org>
2026-07-03 19:39:37 +0200

    [BlockData] Switch to using the StatePredicate instead of MapColor to fix bug
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@mailbox.org>

--- src/main/java/com/bergerkiller/bukkit/common/internal/logic/BlockDataWrapperHook_Impl_26_1.java
+++ src/main/java/com/bergerkiller/bukkit/common/internal/logic/BlockDataWrapperHook_Impl_26_1.java
@@ -1,14 +1,12 @@
 package com.bergerkiller.bukkit.common.internal.logic;
 
 import com.bergerkiller.bukkit.common.utils.CommonUtil;
-import com.bergerkiller.bukkit.common.utils.LogicUtil;
 import com.bergerkiller.bukkit.common.wrappers.BlockData;
 import com.bergerkiller.mountiplex.reflection.resolver.Resolver;
 import com.bergerkiller.mountiplex.reflection.util.ExtendedClassWriter;
 import com.bergerkiller.mountiplex.reflection.util.FastField;
 import com.bergerkiller.mountiplex.reflection.util.NullInstantiator;
 import com.bergerkiller.mountiplex.reflection.util.asm.MPLType;
-import com.bergerkiller.mountiplex.reflection.util.fast.ClassFieldCopier;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.MethodVisitor;
@@ -16,12 +14,13 @@ import org.objectweb.asm.MethodVisitor;
 import static org.objectweb.asm.Opcodes.*;
 
 /**
- * Hooks the 'mapColor' field of the BlockState (BlockBehaviour.BlockStateBase) class to store
- * our own BlockData instance. Used on 26.1 and later.
+ * Hooks the 'isSuffocating' StatePredicate field of the BlockState (BlockBehaviour.BlockStateBase) class to store
+ * our own BlockData instance. Used on 26.1 and later. We implement the interface instead of extending the
+ * original type, as we do not know the (lambda) type at runtime. Instead, simply call the original lambda
+ * from our custom one.
  */
 class BlockDataWrapperHook_Impl_26_1 extends BlockDataWrapperHook {
-    private final FastField<Object> mapColor = new FastField<>();
-    private ClassFieldCopier<Object> mapColorFieldCopier = null;
+    private final FastField<Object> statePredicateField = new FastField<>();
     private NullInstantiator<Object> hookBuilder = null;
     private final FastField<Object> hookBaseField = new FastField<>();
     private final FastField<BlockData> hookBlockDataField = new FastField<>();
@@ -29,54 +28,77 @@ class BlockDataWrapperHook_Impl_26_1 extends BlockDataWrapperHook {
     @Override
     protected void baseEnable() throws Throwable {
         final Class<?> blockStateBaseType = getClassVerify("net.minecraft.world.level.block.state.BlockBehaviour$BlockStateBase");
-        final Class<?> mapColorType = getClassVerify("net.minecraft.world.level.material.MapColor");
+        final Class<?> statePredicateType = getClassVerify("net.minecraft.world.level.block.state.BlockBehaviour$StatePredicate");
 
-        mapColor.init(Resolver.resolveAndGetDeclaredField(blockStateBaseType, "mapColor"));
-        mapColor.forceInitialization();
+        // Hook the field that is likely to be called less. All it adds is a field redirection but it's still good to be mindful.
+        statePredicateField.init(Resolver.resolveAndGetDeclaredField(blockStateBaseType, "isSuffocating"));
+        statePredicateField.forceInitialization();
 
-        // Generate a new class that extends and hooks MapColor, adding a field to store the BlockData
-        final ExtendedClassWriter<Object> cw = ExtendedClassWriter.builder(mapColorType)
-                .addInterface(Accessor.class)
-                .setFlags(ClassWriter.COMPUTE_MAXS)
-                .setClassLoader(BlockDataWrapperHook_Impl_26_1.class.getClassLoader())
-                .build();
-        final String blockDataDesc = MPLType.getDescriptor(BlockData.class);
-        final String mapColorDesc = MPLType.getDescriptor(mapColorType);
-
-        // Add the fields holding the original value and the BlockData to be stored
+        // Generate a new class that implements the StatePredicate interface and calls the original method on the original value
+        Class<?> type;
         {
-            FieldVisitor fv = cw.visitField(ACC_PRIVATE | ACC_FINAL, "base", mapColorDesc, null, null);
-            fv.visitEnd();
-        }
-        {
-            FieldVisitor fv = cw.visitField(ACC_PRIVATE | ACC_FINAL, "blockData", blockDataDesc, null, null);
-            fv.visitEnd();
-        }
+            final ExtendedClassWriter<Object> cw = ExtendedClassWriter.builder(statePredicateType)
+                    .addInterface(Accessor.class)
+                    .setFlags(ClassWriter.COMPUTE_MAXS)
+                    .setClassLoader(BlockDataWrapperHook_Impl_26_1.class.getClassLoader())
+                    .build();
+            final String blockDataDesc = MPLType.getDescriptor(BlockData.class);
+            final String statePredicateDesc = MPLType.getDescriptor(statePredicateType);
+            final String nmsBlockStateDesc = MPLType.getDescriptor(getClassVerify("net.minecraft.world.level.block.state.BlockState"));
+            final String nmsBlockGetterDesc = MPLType.getDescriptor(getClassVerify("net.minecraft.world.level.BlockGetter"));
+            final String nmsBlockPosDesc = MPLType.getDescriptor(getClassVerify("net.minecraft.core.BlockPos"));
 
-        // Implement the Accessor interface
-        {
-            MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "bkcGetOriginalValue", "()Ljava/lang/Object;", null, null);
-            mv.visitCode();
-            mv.visitVarInsn(ALOAD, 0);
-            mv.visitFieldInsn(GETFIELD, cw.getInternalName(), "base", mapColorDesc);
-            mv.visitInsn(ARETURN);
-            mv.visitMaxs(1, 1);
-            mv.visitEnd();
-        }
-        {
-            MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "bkcGetBlockData", "()" + blockDataDesc, null, null);
-            mv.visitCode();
-            mv.visitVarInsn(ALOAD, 0);
-            mv.visitFieldInsn(GETFIELD, cw.getInternalName(), "blockData", blockDataDesc);
-            mv.visitInsn(ARETURN);
-            mv.visitMaxs(1, 1);
-            mv.visitEnd();
-        }
+            // Add the fields holding the original value and the BlockData to be stored
+            {
+                FieldVisitor fv = cw.visitField(ACC_PRIVATE | ACC_FINAL, "base", statePredicateDesc, null, null);
+                fv.visitEnd();
+            }
+            {
+                FieldVisitor fv = cw.visitField(ACC_PRIVATE | ACC_FINAL, "blockData", blockDataDesc, null, null);
+                fv.visitEnd();
+            }
 
-        Class<?> type = cw.generate();
+            // Implement the Accessor interface
+            {
+                MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "bkcGetOriginalValue", "()Ljava/lang/Object;", null, null);
+                mv.visitCode();
+                mv.visitVarInsn(ALOAD, 0);
+                mv.visitFieldInsn(GETFIELD, cw.getInternalName(), "base", statePredicateDesc);
+                mv.visitInsn(ARETURN);
+                mv.visitMaxs(1, 1);
+                mv.visitEnd();
+            }
+            {
+                MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "bkcGetBlockData", "()" + blockDataDesc, null, null);
+                mv.visitCode();
+                mv.visitVarInsn(ALOAD, 0);
+                mv.visitFieldInsn(GETFIELD, cw.getInternalName(), "blockData", blockDataDesc);
+                mv.visitInsn(ARETURN);
+                mv.visitMaxs(1, 1);
+                mv.visitEnd();
+            }
+
+            //boolean test(BlockState state, BlockGetter level, BlockPos pos);
+            {
+                final String testMethodDesc =  "(" + nmsBlockStateDesc + nmsBlockGetterDesc + nmsBlockPosDesc + ")Z";
+
+                MethodVisitor mv =  cw.visitMethod(ACC_PUBLIC, "test", testMethodDesc, null, null);
+                mv.visitCode();
+                mv.visitVarInsn(ALOAD, 0);
+                mv.visitFieldInsn(GETFIELD, cw.getInternalName(), "base", statePredicateDesc);
+                mv.visitVarInsn(ALOAD, 1);
+                mv.visitVarInsn(ALOAD, 2);
+                mv.visitVarInsn(ALOAD, 3);
+                mv.visitMethodInsn(INVOKEINTERFACE, MPLType.getInternalName(statePredicateType), "test", testMethodDesc, true);
+                mv.visitInsn(IRETURN);
+                mv.visitMaxs(4, 4);
+                mv.visitEnd();
+            }
+
+            type = cw.generate();
+        }
 
         hookBuilder = NullInstantiator.of(type);
-        mapColorFieldCopier = ClassFieldCopier.of(LogicUtil.unsafeCast(mapColorType));
         hookBaseField.init(type.getDeclaredField("base"));
         hookBlockDataField.init(type.getDeclaredField("blockData"));
     }
@@ -91,18 +113,17 @@ class BlockDataWrapperHook_Impl_26_1 extends BlockDataWrapperHook {
 
     @Override
     public Object getAccessor(Object nmsIBlockData) {
-        return mapColor.get(nmsIBlockData);
+        return statePredicateField.get(nmsIBlockData);
     }
 
     @Override
     protected void setAccessor(Object nmsIBlockdata, Object accessor) {
-        mapColor.set(nmsIBlockdata, accessor);
+        statePredicateField.set(nmsIBlockdata, accessor);
     }
 
     @Override
     protected Object hook(Object accessor, BlockData blockData) {
         Object hook = hookBuilder.create();
-        mapColorFieldCopier.copy(accessor, hook);
         hookBaseField.set(hook, accessor);
         hookBlockDataField.set(hook, blockData);
         return hook;
