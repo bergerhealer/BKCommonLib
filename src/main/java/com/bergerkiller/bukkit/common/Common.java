eb6219b77b12652511f21c9fdeb5d87febda2a0f
Irmo van den Berge <irmo.vandenberge@mailbox.org>
2026-07-03 18:22:28 +0200

    Forgot common version bump
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@mailbox.org>

--- src/main/java/com/bergerkiller/bukkit/common/Common.java
+++ src/main/java/com/bergerkiller/bukkit/common/Common.java
@@ -95,7 +95,7 @@ public class Common {
      * <b>Use getVersion() instead if you want the actual, current version!
      * Constants get inlined when compiling!</b>
      */
-    public static final int VERSION = 20000;
+    public static final int VERSION = 20001;
     /**
      * Defines the Minecraft version that runs on the server.
      */
